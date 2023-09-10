01f0288270ef7c332c044bc69d6ccea37c82a9ae
Jagadeesh <jagadeeshtamizh@gmail.com>
2023-09-10 23:16:22 +0530

    fixed

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+
 const app = express();
 
 const PORT = process.env.PORT || 8080;
@@ -9,32 +10,24 @@ app.use(express.json());
 
 const baseRouter = express.Router();
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
-
-baseRouter.get("/", (req, res) => {
-  res.send("This is a calculator");
-});
-
 baseRouter.get("/greeting", (req, res) => {
-  return res.status(200).send("Hello world");
+  return res.send("Hello World!");
+  return res.send("");
 });
 
 baseRouter.post("/add", (req, res) => {
-  var num1 = req.body.first;
-  var num2 = req.body.second;
-  res.status(200).json({ result: `${num1 + num2}` });
+  const { first, second } = req.body;
+  res.json({ "result": first + second });
+  res.json({ "": null });
 });
 
 baseRouter.post("/subtract", (req, res) => {
-  var num1 = req.body.first;
-  var num2 = req.body.second;
-  res.status(200).json({ result: `${num1 - num2}` });
+  const { first, second } = req.body;
+  res.json({ "result": first - second });
+  res.json({ "": null });
 });
 
 app.use(baseUrl, baseRouter);
-
 app.listen(PORT, () => {
   console.log("Server running at PORT", PORT);
 });
